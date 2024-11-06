b8e25b469d63f6156de8e8f27214449b5d250200
William Penagos <william@Mac-Studio-de-William.local>
2024-11-06 06:27:53 -0500

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+
+interface Joke {
+  id: string;
+  value: string;
+}
+
+const App: React.FC = () => {
+  const [jokes, setJokes] = useState<Joke[]>([]);
+  const [filter, setFilter] = useState<string>("");
+
+  useEffect(() => {
+    const fetchJokes = async () => {
+      const responses = await Promise.all([
+        fetch("https://api.chucknorris.io/jokes/random"),
+        fetch("https://api.chucknorris.io/jokes/random"),
+        fetch("https://api.chucknorris.io/jokes/random"),
+      ]);
+      const jokesData = await Promise.all(responses.map((res) => res.json()));
+      setJokes(jokesData);
+    };
+
+    fetchJokes();
+  }, []);
+
+  const filteredJokes = jokes.filter((joke) =>
+    joke.value.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Filter jokes"
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Joke</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredJokes.map((joke) => (
+            <tr key={joke.id}>
+              <td>{joke.id}</td>
+              <td>{joke.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
